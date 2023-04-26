7cbe3bfd52f7f11331c5916896dfaad24888ffb9
MechelleGG <752429@student.dusd.net>
2023-04-26 09:56:00 -0700

    Added more movement

--- PlayerControllerLabGaldamez/Assets/Scripts/PlayerController.cs
+++ PlayerControllerLabGaldamez/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 100.0f;
     private Rigidbody playerRb;
+    private float zBound = 6;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
 
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
---
+++ PlayerControllerLabGaldamez/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject powerup;
+    public GameObject[] obstacle;
+    public GameObject Health;
+    private float zEnemySpawn = 12.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
